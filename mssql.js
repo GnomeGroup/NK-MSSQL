7c7ad421f6fc9b88e0701028a233785d45367d88
Encke Technologies <james@encke.com>
2020-08-11 14:20:25 -0600

    1.0.17

--- mssql.js
+++ mssql.js
@@ -10,16 +10,21 @@ const mssqlDBJSObject = {
 	updateList: {},
 	databaseList: {},
 	id: name => objectid( name ),
-	run: async ( dbName, sql, callback ) => {
+	run: ( dbName, sql, callback ) => {
 		if( mssqlDBJSObject.databaseList[dbName] )  {
-			let result = await mssqlDBJSObject.databaseList[dbName].query( sql )
-			if( !result.err && result.res.recordsets )	{
-				callback( result.res.recordsets[0] )
-			} else {
-				callback( [] )
-			}
+			mssqlDBJSObject.databaseList[dbName].query( sql )
+				.then( result => {
+					if( !result.err && result.res.recordsets )	{
+						callback( result.res.recordsets[0], null )
+					} else {
+						callback( [], null )
+					}
+				})
+				.catch( err => {
+					callback( [], err )
+				})
 		} else {
-			callback( [] )
+			callback( [], null )
 		}
 	},
 	start: ( dbName, ip, port, user, pass, timeoutInMS, encryptConnection, callback ) => {
@@ -31,13 +36,13 @@ const mssqlDBJSObject = {
 				callback( true, 'cannot connect' )
 			}
 		}).catch( err => {
-			callback( true, err )
+			callback( true, ( ( err && err.originalError && err.originalError.message )? err.originalError.message: err ) )
 		})
 	},
 	insert: ( dbName, table, rowOrRows, callback ) => mssqlDBJSObject.run( dbName, NKSQL.insert( table, rowOrRows ), callback ),
 	delete: ( dbName, table, dataToRemove, callback ) => mssqlDBJSObject.run( dbName, NKSQL.delete( table, dataToRemove ), callback ),
 	update: ( dbName, table, dataToUpdate, newData, callback ) => mssqlDBJSObject.run( dbName, NKSQL.update( table, newData, dataToUpdate ), callback ),
-	singleQuery: ( dbName, table, query, callback ) => mssqlDBJSObject.run( dbName, NKSQL.query( table, 1, {}, query, null ), ( rows ) => callback( ( rows && ( rows.length > 0 ) )? rows[0]: null ) ),
+	singleQuery: ( dbName, table, query, callback ) => mssqlDBJSObject.run( dbName, NKSQL.query( table, 1, {}, query, null ), ( rows, error ) => callback( ( ( rows && ( rows.length > 0 ) )? rows[0]: null ), error ) ),
 	query: ( dbName, table, query, callback ) => mssqlDBJSObject.run( dbName, NKSQL.query( table, null, {}, query, null ), callback ),
 	join: ( dbName, table, tableIDField, joinTo, joinToIDField, joinedToElement, sortBy, query, callback ) => mssqlDBJSObject.run( dbName, NKSQL.query( table, null, sortBy, query, [ { from: joinTo, field: tableIDField, fromField: joinToIDField, name: joinedToElement } ] ), callback ),
 	joinsLimit: ( dbName, table, joins, max, sortBy, query, callback ) => mssqlDBJSObject.run( dbName, NKSQL.query( table, max, sortBy, query, joins ), callback ),
